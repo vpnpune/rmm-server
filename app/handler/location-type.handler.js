059643c7799356d8ba453528d93f7742acc0fab7
neerajrd25 <kumaratneeraj@gmail.com>
2018-10-02 11:25:18 +0530

    Merged Conflicts

---
+++ app/handler/location-type.handler.js
@@ -0,0 +1,67 @@
+import { DatabaseService } from "../db/database.service";
+import * as Collection from '../db/collection-constants';
+import mongodb from "../db/mongodb";
+
+
+/* SET COLLECTION NAME FIRST*/
+const collectionName = Collection.LOCATION_TYPE;
+
+const ObjectId = require('mongodb').ObjectID;
+
+export class LocationTypeHandler {
+    // get all items from collection
+    static async getAll() {
+        try {
+            let result = await DatabaseService.getAll(collectionName);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+
+    }
+    // get ONE object from db
+    static async getOne(id) {
+        try {
+           let result = await DatabaseService.getOne(collectionName, ObjectId(id));
+            return result;
+        } catch (err) {
+            throw err;
+        }
+
+    }
+    // save object to db
+    static async save(data) {
+        try {
+
+            let result = await DatabaseService.saveWithObjectId(collectionName, data);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    }
+    // update container
+    static async updateOne(data) {
+        try {
+            data._id= ObjectId(data._id);
+            let result = await DatabaseService.updateOneWithObjectId(collectionName, data);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    }
+    // Delete One container
+    static async deleteOne(id) {
+        try {
+            // send objectId version
+            let result = await DatabaseService.deleteOne(collectionName, id);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    }
+   
+
+}
+
+
+
---
+++ app/routes/location-type.route.js
@@ -0,0 +1,131 @@
+import express from 'express';
+import Joi from 'joi';
+import validator from 'express-joi-validator';
+import { LocationTypeHandler } from '../handler/location-type.handler';
+import logger from '../logger';
+
+
+const log = logger.Logger;
+
+var data = [
+	{
+		"level": 1,
+		"name": "Facility"
+	},
+	{
+		"level": 2,
+		"name": "Site"
+	},
+	{
+		"level": 3,
+		"name": "Warehouse"
+	},
+	{
+		"level": 4,
+		"name": "Room"
+	},
+	{
+		"level": 5,
+		"name": "Section"
+	}
+];
+
+const router = express.Router();
+// please separate  out 
+const schema = {
+	body: {
+		name: Joi.string().min(3).required(),
+		alias: Joi.string().min(1).required(),
+		level: Joi.number().required()
+	}
+}
+
+// get ALL
+router.get('/', (req, res) => {
+	let resultPromise = LocationTypeHandler.getAll();
+	resultPromise.then(function (result) {
+		if (result) {
+			res.status(200).send(result);
+		} else {
+			res.status(200).send([]);
+		}
+	}).catch(err => {
+		log.error(err);
+		res.status(500).send({ "message": "Something went wrong" });
+	});
+});
+
+
+// get ONE
+router.get('/:id', (req, res) => {
+	let id = req.params.id;
+
+	let resultPromise = LocationTypeHandler.getOne(id);
+	resultPromise.then(function (result) {
+		if (result) {
+			res.status(200).send(result);
+		} else {
+			res.status(200).send([]);
+		}
+	}).catch(err => {
+		log.error(err);
+		res.status(500).send({ "message": "Something went wrong" });
+	});
+});
+
+
+
+// save obj
+router.post('/', validator(schema, { allowUnknown: true, abortEarly: false }), (req, res, next) => {
+	let resultPromise = LocationTypeHandler.save(req.body);
+	resultPromise.then(function (result) {
+		if (result) {
+			res.status(200).send(result);
+		} else {
+			res.status(200).send([]);
+		}
+	}).catch(err => {
+		log.error(err);
+		res.status(500).send({ "message": "Something went wrong" });
+	});
+
+});
+
+// update ONE obj
+router.put('/', validator(schema, { allowUnknown: true, abortEarly: false }), (req, res, next) => {
+	console.log("Router put");
+	let resultPromise = LocationTypeHandler.updateOne(req.body);
+
+	resultPromise.then(function (result) {
+		if (result) {
+			res.status(200).send(result);
+		} else {
+			res.status(200).send([]);
+		}
+	}).catch(err => {
+		log.error(err);
+		res.status(500).send({ "message": "Something went wrong" });
+	});
+
+});
+
+
+// get ONE
+router.delete('/:id', (req, res) => {
+	let id = req.params.id;
+	console.log("Delete Route Called");
+	let resultPromise = LocationTypeHandler.deleteOne(id);
+	resultPromise.then(function (result) {
+		if (result) {
+			res.status(200).send(result);
+		} else {
+			res.status(200).send([]);
+		}
+	}).catch(err => {
+		log.error(err);
+		res.status(500).send({ "message": "Something went wrong" });
+	});
+});
+
+
+export default router;
