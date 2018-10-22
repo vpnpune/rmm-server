2fec80e66f87dba3daeacb9439bbd9dee56d3aed
pankajsaboo <pankaj.saboo08@gmail.com>
2018-10-22 12:00:35 +0530

    constant changes

--- app/constants.js
+++ app/constants.js
@@ -1,11 +1,11 @@
 const PORT = process.env.PORT || 3000; // port to start server on
 const MONGO_PORT = process.env.DB_PORT || 27017; // mongo port
-const DB_NAME = process.env.DB_NAME; // sample db name
-const SECRET = process.env.SECRET;
+const DB_NAME = process.env.DB_NAME || 'LAB_MANUS'; // sample db name
+const SECRET = process.env.SECRET || "RMMSECRET";
 const REDIS_PORT = 3030;
-const DB_URL = process.env.DB_URL;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DB_URL || 'localhost';
+const DB_USERNAME = process.env.DB_USERNAME || '';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
 
 //Error Message
 const TOKEN_VERIFICATION_FAILED = "Token verification failed. Please try again.";
@@ -34,5 +34,8 @@ export {
 	LOCAL_PORT,
 	TEST_BASE_URL,
 	TEST_DB_URL,
-	DEV_ENV
+	DEV_ENV,
+	DB_URL,
+	DB_USERNAME,
+	DB_PASSWORD
 }
