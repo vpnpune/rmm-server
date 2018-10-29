a6a12709be2adb51cfe0462b8edef90eb2bc8728
Pankaj Saboo <pankaj.saboo08@gmail.com>
2018-10-29 08:39:33 +0530

    file upload changes

--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -6,52 +6,36 @@ import Document from '../model/document';
 import { DocumentHandler } from '../handler/document.handler';
 const router = express.Router();
 const log = logger.Logger;
+const fs = require('fs');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3')
 
-let upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/profile', upload.any(), function (req, res, next) {
-  console.log(req.files);
-  if(req.files.length > 0) {
-    let document = new Document();
-    document.userId = app.get('user');
-    document.fileName = req.files[0].originalname;
-    document.mimeType = req.files[0].mimetype;
-    document.data = req.files[0].buffer.toString();
-    console.log(req.files[0]);
-    
-    let resultPromise = DocumentHandler.save(document);
-	  resultPromise.then(function (result) {
-		if (result) {
-			res.status(200).send(result);
-		} else {
-			res.status(200).send([]);
-		}
-	}).catch(err => {
-		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
-	});
-  } else {
-    return res.status(400).send("Bad Request");
-  }
-  
+
+aws.config.update({
+  accessKeyId: 'XXXX',
+  secretAccessKey: 'XXXX',
+  region: 'XXXX'
+});
+var s3 = new aws.S3();
+
+
+var upload = multer({
+  storage: multerS3({
+      s3: s3,
+      bucket: 'rmm-s3-store',
+      key: function (req, file, cb) {
+          console.log("file: ",file);
+          cb(null, file.originalname); //use Date.now() for unique file keys
+      }
+  })
 });
 
-router.get('/download/:id', function(req, res, next) {
-  let id = req.params.id;
-  console.log("client id " + id)
-	let resultPromise = DocumentHandler.getOne(id);
-	
-	resultPromise.then(function (result) {
-    console.log("Router result: ",result._fileName);
-		if (result) {
-      console.log(Buffer.from(result._data));
-      res.type(result._mimeType).send(Buffer.from(result._data,"base64"));
-		}
-	}).catch(err => {
-    console.log(err);
-		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
-	});
+
+//use by upload form
+router.post('/profile', upload.any(), function (req, res, next) {
+  console.log(res);
+  res.send("Uploaded!");
 });
 
 export default router;
