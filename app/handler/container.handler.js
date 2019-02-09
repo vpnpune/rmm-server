ed826b3c99f1caf7b1dd23091e3486d195e1ca97
Pankaj Saboo <pankaj.saboo08@gmail.com>
2019-02-10 00:32:39 +0530

    counter service

--- app/handler/container.handler.js
+++ app/handler/container.handler.js
@@ -1,6 +1,6 @@
 import { DatabaseService } from "../db/database.service";
 import * as Collection from '../db/collection-constants';
-
+import {CounterService} from "../db/counter-service";
 /* SET COLLECTION NAME FIRST*/
 const collectionName = Collection.CONTAINER_TYPE;
 
