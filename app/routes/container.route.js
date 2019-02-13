7e7a4b37a3c59662def421c2c49cf1fe0156ba7a
Neeraj <getnvkarma@gmail.com>
2019-02-13 08:29:11 +0530

    error messages

--- app/routes/container.route.js
+++ app/routes/container.route.js
@@ -2,9 +2,15 @@ import express from 'express';
 import Joi from 'joi';
 import validator from 'express-joi-validator';
 import logger from '../logger';
-import { ContainerHandler } from '../handler/container.handler';
-import { UserHandler } from '../handler/user.handler';
-import { RolesHandler } from '../handler/roles.handler';
+import {
+	ContainerHandler
+} from '../handler/container.handler';
+import {
+	UserHandler
+} from '../handler/user.handler';
+import {
+	RolesHandler
+} from '../handler/roles.handler';
 import app from './../server';
 
 const router = express.Router();
@@ -13,8 +19,17 @@ const log = logger.Logger;
 // please separate  out 
 const schema = {
 	body: {
-		containerName: Joi.string().min(3).max(20).required(),
-		prefix: Joi.string().max(4).required()
+		containerName: Joi.string().min(3).max(20).required().error(errors => {
+
+			return {
+				message: "Name must be min 3 and max 20 charachters long."
+			};
+		}),
+		prefix: Joi.string().max(4).required().error(errors => {
+			return {
+				message: "Prefix cannot be more than 4 charachters."
+			};
+		}),
 	}
 }
 // get ALL
@@ -28,7 +43,9 @@ router.get('/', (req, res) => {
 		}
 	}).catch(err => {
 		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
+		res.status(500).send({
+			"message": "Something went wrong"
+		});
 	});
 });
 
@@ -46,14 +63,19 @@ router.get('/:id', (req, res) => {
 		}
 	}).catch(err => {
 		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
+		res.status(500).send({
+			"message": "Something went wrong"
+		});
 	});
 });
 
 
 
 // save obj
-router.post('/', validator(schema, { allowUnknown: true, abortEarly: false }), (req, res, next) => {
+router.post('/', validator(schema, {
+	allowUnknown: true,
+	abortEarly: false
+}), (req, res, next) => {
 	let resultPromise = ContainerHandler.save(req.body);
 	resultPromise.then(function (result) {
 		if (result) {
@@ -63,14 +85,19 @@ router.post('/', validator(schema, { allowUnknown: true, abortEarly: false }), (
 		}
 	}).catch(err => {
 		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
+		res.status(500).send({
+			"message": "Something went wrong"
+		});
 	});
 
 });
 
 // update ONE obj
-router.put('/', validator(schema, { allowUnknown: true, abortEarly: false }), (req, res, next) => {
-	
+router.put('/', validator(schema, {
+	allowUnknown: true,
+	abortEarly: false
+}), (req, res, next) => {
+
 	let resultPromise = ContainerHandler.updateOne(req.body);
 
 	resultPromise.then(function (result) {
@@ -81,7 +108,9 @@ router.put('/', validator(schema, { allowUnknown: true, abortEarly: false }), (r
 		}
 	}).catch(err => {
 		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
+		res.status(500).send({
+			"message": "Something went wrong"
+		});
 	});
 
 });
@@ -90,7 +119,7 @@ router.put('/', validator(schema, { allowUnknown: true, abortEarly: false }), (r
 // get ONE
 router.delete('/:id', (req, res) => {
 	let id = req.params.id;
-	
+
 	let resultPromise = ContainerHandler.deleteOne(id);
 	resultPromise.then(function (result) {
 		if (result) {
@@ -100,7 +129,9 @@ router.delete('/:id', (req, res) => {
 		}
 	}).catch(err => {
 		log.error(err);
-		res.status(500).send({ "message": "Something went wrong" });
+		res.status(500).send({
+			"message": "Something went wrong"
+		});
 	});
 });
 
@@ -112,13 +143,13 @@ function checkForPermissions(permissionName) {
 	// 	if (result) {
 	// 		let user = result;
 	// 		console.log("User: ",user);
-			// let permissions = user.permissions;
-			// console.log("check permission: Permissions: ",permissions);
-			// if(permissions.includes(permissionName)){
-			// 	return true;
-			// } else {
-			// 	return false;
-			// }
+	// let permissions = user.permissions;
+	// console.log("check permission: Permissions: ",permissions);
+	// if(permissions.includes(permissionName)){
+	// 	return true;
+	// } else {
+	// 	return false;
+	// }
 	// 	} else {
 	// 		log.error("user exist error: ",resut);
 	// 		return false;
